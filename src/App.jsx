fc0fcb0181ab174900a30bff0f0a0b9a095f30ca
Ehabel <eelcheikh@hotmail.com>
2023-04-19 23:35:05 +1000

    Added styling and product pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
 import "./App.css";
-import { addProducts, getAllProducts } from "./services/products";
+import ProductsList from "./containers/ProductsList/ProductsList";
+import Product from "./components/Product/Product";
+import { BrowserRouter } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 import { useState, useEffect } from "react";
+import { addProducts, getAllProducts } from "./services/products";
+import ProductCard from "./components/ProductCard/ProductCard";
 
 function App() {
-    // const [products, setProducts] = useState([]);
-    // useEffect(() => {
-    //     const wrapper = async () => {
-    //         setProducts(await getAllProducts());
-    //     };
-    //     wrapper();
-    // }, []);
-    // console.log(products);
-    // addProducts();
-    return <div className="App"></div>;
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        console.log("use effect called");
+        const getProducts = async () => {
+            setProducts(await getAllProducts());
+        };
+        getProducts();
+    }, []);
+    return (
+        <div className="App">
+            <BrowserRouter basename="/eshop">
+                <div className="navbar">
+                    <h3 className="heading">EShop</h3>
+                    <Nav />
+                </div>
+                <div className="container">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Home products={products} />}
+                        />
+                        <Route
+                            path="/products"
+                            element={<ProductsList products={products} />}
+                        />
+                        <Route path="/products/:id" element={<ProductCard />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/services/products.js
+++ src/services/products.js
@@ -11,63 +11,113 @@ import { db } from "../firebase";
 
 const shoes = [
     {
-        id: 1,
-        title: "Casual Sneakers",
-        price: 200,
+        id: 7,
+        title: "Black Puma",
+        price: 250,
         description: "",
-        image: "../../images/casualSneaker.jpg",
+        image: "../../images/blackPuma.jpg",
         variants: ["XS", "S", "M", "L", "XL", "XXL"],
-        quantities: { XS: 3, S: 0, M: 2, L: 8, XL: 2, XXL: 6 },
+        quantities: { XS: 13, S: 10, M: 12, L: 8, XL: 2, XXL: 6 },
         favourite: false,
     },
     {
-        id: 2,
-        title: "Leather Dress Shoes",
-        price: 300,
+        id: 8,
+        title: "Blue Nike Jordans",
+        price: 500,
         description: "",
-        image: "../../images/dressShoes.jpg",
+        image: "../../images/blueJordans.jpg",
         variants: ["XS", "S", "M", "L", "XL", "XXL"],
-        quantities: { XS: 5, S: 4, M: 20, L: 18, XL: 3, XXL: 1 },
+        quantities: { XS: 15, S: 41, M: 10, L: 8, XL: 3, XXL: 1 },
         favourite: false,
     },
     {
-        id: 3,
-        title: "Black HighTop LED Sneakers",
-        price: 450,
+        id: 9,
+        title: "Brown Boots",
+        price: 350,
         description: "",
-        image: "../../images/ledShoes.jpg",
+        image: "../../images/boots.jpg",
         variants: ["XS", "S", "M", "L", "XL", "XXL"],
-        quantities: { XS: 1, S: 3, M: 7, L: 3, XL: 12, XXL: 16 },
+        quantities: { XS: 13, S: 31, M: 17, L: 4, XL: 12, XXL: 16 },
         favourite: false,
     },
     {
-        id: 4,
-        title: "Red Hightop Sneakers",
-        price: 550,
+        id: 10,
+        title: "Brown Dress Shoes",
+        price: 575,
         description: "",
-        image: "../../images/newShoes.jpg",
+        image: "../../images/brownDress.jpg",
         variants: ["XS", "S", "M", "L", "XL", "XXL"],
-        quantities: { XS: 3, S: 6, M: 2, L: 4, XL: 8, XXL: 16 },
+        quantities: { XS: 4, S: 16, M: 2, L: 14, XL: 18, XXL: 16 },
         favourite: false,
     },
     {
-        id: 5,
-        title: "Blue Hightop Sneakers",
-        price: 550,
+        id: 11,
+        title: "Differente",
+        price: 100,
         description: "",
-        image: "../../images/newShoesTwo.jpg",
+        image: "../../images/differente.jpg",
         variants: ["XS", "S", "M", "L", "XL", "XXL"],
-        quantities: { XS: 2, S: 1, M: 7, L: 3, XL: 4, XXL: 5 },
+        quantities: { XS: 21, S: 13, M: 74, L: 30, XL: 41, XXL: 5 },
         favourite: false,
     },
     {
-        id: 6,
-        title: "Black Runners",
-        price: 150,
+        id: 12,
+        title: "Yellow Dr Martens",
+        price: 275,
         description: "",
-        image: "../../images/runners.jpg",
+        image: "../../images/drMartens.jpg",
         variants: ["XS", "S", "M", "L", "XL", "XXL"],
-        quantities: { XS: 1, S: 13, M: 3, L: 8, XL: 15, XXL: 12 },
+        quantities: { XS: 11, S: 1, M: 7, L: 3, XL: 5, XXL: 12 },
+        favourite: false,
+    },
+    {
+        id: 13,
+        title: "Gold Nike",
+        price: 1000,
+        description: "",
+        image: "../../images/goldNike.jpg",
+        variants: ["XS", "S", "M", "L", "XL", "XXL"],
+        quantities: { XS: 2, S: 1, M: 1, L: 2, XL: 15, XXL: 13 },
+        favourite: false,
+    },
+    {
+        id: 14,
+        title: "Black Loafers",
+        price: 675,
+        description: "",
+        image: "../../images/loafers.jpg",
+        variants: ["XS", "S", "M", "L", "XL", "XXL"],
+        quantities: { XS: 11, S: 11, M: 17, L: 13, XL: 15, XXL: 12 },
+        favourite: false,
+    },
+    {
+        id: 15,
+        title: "Puma",
+        price: 225,
+        description: "",
+        image: "../../images/puma.jpg",
+        variants: ["XS", "S", "M", "L", "XL", "XXL"],
+        quantities: { XS: 10, S: 9, M: 7, L: 3, XL: 10, XXL: 12 },
+        favourite: false,
+    },
+    {
+        id: 16,
+        title: "Red Casual Sneakers",
+        price: 235,
+        description: "",
+        image: "../../images/redSneakers.jpg",
+        variants: ["XS", "S", "M", "L", "XL", "XXL"],
+        quantities: { XS: 11, S: 11, M: 70, L: 30, XL: 15, XXL: 14 },
+        favourite: false,
+    },
+    {
+        id: 17,
+        title: "White high top sneakers",
+        price: 350,
+        description: "",
+        image: "../../images/whiteHighTop.jpg",
+        variants: ["XS", "S", "M", "L", "XL", "XXL"],
+        quantities: { XS: 6, S: 9, M: 17, L: 23, XL: 35, XXL: 19 },
         favourite: false,
     },
 ];
@@ -87,4 +137,16 @@ export const addProducts = async () => {
     shoes.map((shoe) => {
         addDoc(collection(db, "products"), shoe);
     });
+};
+
+export const getProduct = async (id) => {
+    const docRef = doc(db, "products", id);
+    const docSnap = await getDoc(docRef);
+    console.log(docRef);
+
+    if (docSnap.exists()) {
+        return { id, ...docSnap.data() };
+    } else {
+        throw new Error("Doc not found");
+    }
 };
