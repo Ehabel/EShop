d9c359867aae34f44ddd8a70b8707f2c2c9c9c83
Ehabel <eelcheikh@hotmail.com>
2023-05-29 10:26:31 +1000

    Fixed carousel issues

--- src/containers/CarouselPage/CarouselPage.jsx
+++ src/containers/CarouselPage/CarouselPage.jsx
@@ -33,28 +33,28 @@ const CarouselPage = () => {
                     <Slide index={0}>
                         <img
                             className={styles.img}
-                            src="../../images/nikebanner.gif"
+                            src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7307e562514417.5a931ab904cad.gif"
                             alt=""
                         />
                     </Slide>
                     <Slide index={1}>
                         <img
                             className={styles.img}
-                            src="../../images/pumaBanner.jpg"
+                            src="https://wallpaperaccess.com/full/1376635.png"
                             alt=""
                         />
                     </Slide>
                     <Slide index={2}>
                         <img
                             className={styles.img}
-                            src="../../images/drMartensBanner.jpg"
+                            src="https://images.squarespace-cdn.com/content/v1/5d7fe48a00c0503e9f33da41/1588730211646-174E9XHWBAM8D9NAJSC3/DrMartens_banner.jpg?format=1500w"
                             alt=""
                         />
                     </Slide>
                     <Slide index={3}>
                         <img
                             className={styles.img}
-                            src="../../images/puma4k.webp"
+                            src="https://cdn.denimandcloth.com.au/content/uploads/2014/11/18121834/adidas-banner.jpg"
                             alt=""
                         />
                     </Slide>
